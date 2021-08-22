c4040f12385c89780c40e93f20f7ba5d6a47b411
s-amano <amahaya0831@gmail.com>
2021-08-22 16:59:19 +0900

    blog detail page

---
+++ pages/blogs/[id].js
@@ -0,0 +1,49 @@
+import { client } from '../../libs/client';
+import Layout from '../../components/Layout';
+import Image from 'next/image';
+
+export default function BlogId({ blog }) {
+  return (
+    <Layout title="blog">
+      <div className="mx-8 my-6 flex flex-1 justify-center items-center flex-col w-screen max-w-2xl">
+        <div className="border-b-2 border-gray-700 m-8">
+          <p className="text-3xl">{blog.title}</p>
+        </div>
+
+        <div className="mt-6 mx-4">
+          <Image className="rounded-3xl" src={blog.image.url} width={400} height={300}></Image>
+        </div>
+
+        <div
+          style={{ marginRight: '5%', marginLeft: '5%' }}
+          className="my-8 shadow-inner rounded-xl bg-white p-6 max-w-full"
+        >
+          <div
+            className="prose"
+            dangerouslySetInnerHTML={{
+              __html: `${blog.body}`,
+            }}
+          />
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: 'blogs' });
+
+  const paths = data.contents.map((content) => `/blogs/${content.id}`);
+  return { paths, fallback: false };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: 'blogs', contentId: id });
+
+  return {
+    props: {
+      blog: data,
+    },
+  };
+};
