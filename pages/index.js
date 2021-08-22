9bba2a50149ee0463ce3ada761161805150aad0b
s-amano <amahaya0831@gmail.com>
2021-08-22 14:48:03 +0900

    profile card

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 import Layout from '../components/Layout';
+import ProfileCard from '../components/ProfileCard';
 
 export default function Home() {
-  return <Layout title="Home"></Layout>;
+  return (
+    <Layout title="Home">
+      <ProfileCard />
+    </Layout>
+  );
 }
