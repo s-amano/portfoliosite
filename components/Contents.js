d9248bd7ae2658cf455dc768e64c7ff5f69ba162
s-amano <amahaya0831@gmail.com>
2021-12-05 21:18:06 +0900

    fix: コンテンツの配布のCSSを修正

--- components/Contents.js
+++ components/Contents.js
@@ -1,19 +1,19 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Contents({ contents, contentName }) {
-  let content_name = '制作物';
-  if (contentName == 'blogs') {
-    content_name = 'ブログ';
+  let content_name = "制作物";
+  if (contentName == "blogs") {
+    content_name = "ブログ";
   }
   return (
     <>
       <p className="text-3xl my-8 text-black">{`${content_name}一覧`}</p>
-      <div className="flex w-9/12 justify-center flex-wrap content-between pl-3">
+      <div className="flex w-9/12 justify-center flex-wrap content-between pl-3 lg:w-2/3 xl:w-1/2">
         {contents.map((content) => {
           const maxLength = 30;
           if (content.title.length > maxLength) {
-            content.title = content.title.substr(0, maxLength) + '...';
+            content.title = content.title.substr(0, maxLength) + "...";
           } else {
             content.title = content.title;
           }
@@ -34,9 +34,13 @@ export default function Contents({ contents, contentName }) {
                     />
                   </div>
                   <div className="px-4 w-56">
-                    <p className="text-sm text-left font-bold">{content.title}</p>
+                    <p className="text-sm text-left font-bold">
+                      {content.title}
+                    </p>
                     <div className="flex pl-2 mt-1 justify-between items-center">
-                      <div className="h-7 p-1 w-52 text-left text-xs text-gray-500">{date}</div>
+                      <div className="h-7 p-1 w-52 text-left text-xs text-gray-500">
+                        {date}
+                      </div>
                     </div>
                   </div>
                 </div>
