8d44dd08b9f797b2729806b3a2200bb1ea5319d1
s-amano <amahaya0831@gmail.com>
2021-12-27 06:19:24 +0900

    fix: ブログ一覧ページへのリンクをnavに設置

--- components/Layout.js
+++ components/Layout.js
@@ -9,19 +9,25 @@ export default function Layout({ children, title = "TOP" }) {
           <div className="flex items-center pl-8 h-14">
             <div className="flex space-x-4">
               <Link href="/">
-                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                <a className="text-gray-300 hover:bg-gray-700 px-2 py-2 rounded">
                   Home
                 </a>
               </Link>
 
               <Link href="/profile">
-                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                <a className="text-gray-300 hover:bg-gray-700 px-2 py-2 rounded">
                   Profile
                 </a>
               </Link>
 
+              <Link href="/blogs">
+                <a className="text-gray-300 hover:bg-gray-700 px-2 py-2 rounded">
+                  Blog
+                </a>
+              </Link>
+
               <Link href="/portfolio">
-                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                <a className="text-gray-300 hover:bg-gray-700 px-2 py-2 rounded">
                   Portfolio
                 </a>
               </Link>
