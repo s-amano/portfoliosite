44aa816b51cc477e8df5d78346a17e4419355012
s-amano <amahaya0831@gmail.com>
2022-03-04 03:05:20 +0900

    再利用しそうな部品をコンポーネント化

--- components/BlogSidebar.tsx
+++ components/BlogSidebar.tsx
@@ -6,6 +6,8 @@ import { TagType } from "types";
 import Link from "next/link";
 import Image from "next/image";
 import { SnsIcon } from "components/SnsIcon";
+import { TagList } from "./TagList";
+import { LatestBlogList } from "./LatestBlogList";
 
 interface Props {
   latestBlog: BlogType[];
@@ -45,53 +47,8 @@ export const BlogSidebar: React.FC<Props> = React.memo((props: Props) => {
           </div>
         </div>
       </div>
-      <div className="shadow-inner rounded-xl bg-white p-4 mt-4">
-        <p className="border-b-2 border-black text-2xl text-black text-left pt-3 mb-6 pb-2">
-          タグ一覧
-        </p>
-        <div className="flex flex-wrap justify-left items-center">
-          {sortedTag.map((tag) => {
-            return (
-              <div className="m-2" key={tag.id}>
-                <Link href={`/tags/${tag.id}`}>
-                  <a className="text-lg text-blue-500 hover:bg-gray-500 rounded">
-                    #{tag.name}
-                    {`(${tag.count})`}
-                  </a>
-                </Link>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-      {/* <LatestBlogList blog={blog} /> */}
-      <div className="shadow-inner rounded-xl bg-white p-4 mt-4">
-        <p className="border-b-2 border-black text-2xl text-black text-left pt-3 mb-6 pb-2">
-          最新記事
-        </p>
-        <div>
-          {latestBlog.map((blog) => {
-            const maxLength = 30;
-            if (blog.title.length > maxLength) {
-              blog.title = blog.title.substring(0, maxLength) + "...";
-            } else {
-              blog.title = blog.title;
-            }
-            return (
-              <Link key={blog.id} href={`/blogs/${blog.id}`} passHref>
-                <a>
-                  <div className="border-b-2 border-gray-300 mb-6">
-                    <p className="text-base text-gray-400">
-                      {blog.createdAt.substring(0, 10)}
-                    </p>
-                    <p className="text-base">{blog.title}</p>
-                  </div>
-                </a>
-              </Link>
-            );
-          })}
-        </div>
-      </div>
+      <TagList sortedTag={sortedTag} />
+      <LatestBlogList latestBlog={latestBlog} />
     </div>
   );
 });
---
+++ components/LatestBlogList.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { BlogType } from "../types";
+import Link from "next/link";
+
+interface Props {
+  latestBlog: BlogType[];
+}
+
+export const LatestBlogList: React.FC<Props> = React.memo((props: Props) => {
+  const { latestBlog } = props;
+  return (
+    <div className="shadow-inner rounded-xl bg-white p-4 mt-4">
+      <p className="border-b-2 border-black text-2xl text-black text-left pt-3 mb-6 pb-2">
+        最新記事
+      </p>
+      <div>
+        {latestBlog.map((blog) => {
+          const maxLength = 30;
+          if (blog.title.length > maxLength) {
+            blog.title = blog.title.substring(0, maxLength) + "...";
+          } else {
+            blog.title = blog.title;
+          }
+          return (
+            <Link key={blog.id} href={`/blogs/${blog.id}`} passHref>
+              <a>
+                <div className="border-b-2 border-gray-300 mb-6">
+                  <p className="text-base text-gray-400">
+                    {blog.createdAt.substring(0, 10)}
+                  </p>
+                  <p className="text-base">{blog.title}</p>
+                </div>
+              </a>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+});
+
+LatestBlogList.displayName = "LatestBlogList";
