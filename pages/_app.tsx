c2cf064e57c012d3f6ada4a7abc8b61e1744a224
s-amano <amahaya0831@gmail.com>
2022-03-04 23:18:12 +0900

    アナリティクスタグを導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
-import 'tailwindcss/tailwind.css'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import "tailwindcss/tailwind.css";
+import * as gtag from "libs/gtag";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
